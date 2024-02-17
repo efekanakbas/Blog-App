005da1aa656e3ebe0ebc6bdaaeaa7ae3b0dda3cd
Efekan Akbaş <efekanakbas98@gmail.com>
2024-02-17 05:11:00 +0300

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import Navbar from "../components/Navbar";
 import TanstackProvider from "../providers/TanstackProvider";
 import { Toaster } from "react-hot-toast";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <TanstackProvider>
+        <SpeedInsights/>
         <Toaster
         position="top-right"
         reverseOrder={false}
