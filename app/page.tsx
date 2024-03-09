ee4218ecd070b51d9a97ec7b588a521df7d1453e
Efekan Akbaş <efekanakbas98@gmail.com>
2024-03-09 18:14:18 +0300

    patch

--- app/page.tsx
+++ app/page.tsx
@@ -7,43 +7,31 @@ import {
 } from "@tanstack/react-query";
 import axios from "axios";
 import type { Metadata } from "next";
-import {getData} from "../utils/CRUD"
-
-
-
-
+import { getData } from "../utils/CRUD";
 
 export const metadata: Metadata = {
   title: "Home",
   description: "Home Page of Blog",
 };
 
-
-
-
-
 export default async function Home() {
-
-
-
   const queryClient = new QueryClient();
   await queryClient.prefetchInfiniteQuery({
     queryKey: ["feeds"],
-    queryFn: ({pageParam}) => {
-    return getData(`feeds?page=${pageParam}&limit=10`);
-  },
-  staleTime: 5000,
-  initialPageParam: 1,
+    queryFn: ({ pageParam }) => {
+      return getData(`feeds?page=${pageParam}&limit=10`);
+    },
+    staleTime: 5000,
+    initialPageParam: 1,
     getNextPageParam: (lastPage, pages) => {
       const morePageExist = lastPage.length === 10;
-      if(!morePageExist) {
+      if (!morePageExist) {
         return;
       }
       return pages.length + 1;
     },
     pages: 1,
   });
-
 
   return (
     <main className="">
@@ -55,4 +43,3 @@ export default async function Home() {
     </main>
   );
 }
-
