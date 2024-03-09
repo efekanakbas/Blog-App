579b2aff34af26fe81eb8491540790ad7245371b
Efekan Akbaş <efekanakbas98@gmail.com>
2024-03-09 16:49:26 +0300

    patch

--- utils/CRUD.js
+++ utils/CRUD.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // Axios istemcisini oluştur ve yapılandır
 const createNetworkClient = () => {
   const network = axios.create({
-    baseURL: "https://192.168.1.37:5000/",
+    baseURL: "https://localhost:5000/",
   });
 
   // İstekleri yakalama ve token ekleme
