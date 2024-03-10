0a04ef0ff887f93c01fd40e7ce52ab440981cb7a
Efekan Akbaş <efekanakbas98@gmail.com>
2024-03-10 04:37:01 +0300

    deneme

--- app/page.tsx
+++ app/page.tsx
@@ -15,23 +15,25 @@ export const metadata: Metadata = {
 };
 
 export default async function Home() {
-  const queryClient = new QueryClient();
-  await queryClient.prefetchInfiniteQuery({
-    queryKey: ["feeds"],
-    queryFn: ({ pageParam }) => {
-      return getData(`feeds?page=${pageParam}&limit=10`);
-    },
-    staleTime: 5000,
-    initialPageParam: 1,
-    getNextPageParam: (lastPage, pages) => {
-      const morePageExist = lastPage.length === 10;
-      if (!morePageExist) {
-        return;
-      }
-      return pages.length + 1;
-    },
-    pages: 1,
-  });
+   //! States
+   const queryClient = new QueryClient();
+   await queryClient.prefetchInfiniteQuery({
+     queryKey: ["feeds"],
+     queryFn: ({pageParam}) => {
+     return getData(`feeds?page=${pageParam}&limit=10`);
+   },
+   staleTime: 5000,
+   initialPageParam: 1,
+     getNextPageParam: (lastPage, pages) => {
+       const morePageExist = lastPage.length === 10;
+       if(!morePageExist) {
+         return;
+       }
+       return pages.length + 1;
+     },
+     pages: 1,
+   });
+   //!
 
   return (
     <main className="">
